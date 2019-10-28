b959d4a95ffe6508ad22c16445780a3d9ae3936a
katznboyz <38505228+katznboyz1@users.noreply.github.com>
2019-10-28 16:25:08 -0400

    Finished the function "createBillboardImage"

--- desktop-wallpaper.py
+++ desktop-wallpaper.py
@@ -8,8 +8,6 @@ screenSize[0], screenSize[1] = int(screenSize[0]), int(screenSize[1])
 #calculate the artistic vanishing point of the screen
 vainishingPoint = [int(screenSize[0] / 2), int(screenSize[1] / 2)]
 
-#create a function to calculate the edge of a line that is rotated at a certain degree
-
 #function to create the image of the billboard that the time will be on (returns the path to the output)
 def createBillboardImage(path) -> str:
 
@@ -43,11 +41,21 @@ def createBillboardImage(path) -> str:
     billboardTextPadding = [int(screenSize[0] / 25), int(screenSize[0] / 25)]
     billboardSize = [int(textSize[0] + (billboardTextPadding[0] * 2)), int(textSize[1] + (billboardTextPadding[1] * 2))]
     billboardTopCoords = [int((screenSize[0] - billboardSize[0]) / 2), int((screenSize[1] - billboardSize[1]) / 3)]
-    billboardLegHeight = int(billboardSize[1] / 3)
+    billboardLegHeight = int(billboardSize[1] / 1.5)
+    billboardTextLegPositionsX = [int(billboardSize[0] / 4), int((billboardSize[0] / 4) * 3)]
 
-    #create the border lines
+    #create the border lines and sign background
     draw.rectangle([int(billboardTopCoords[0]), int(billboardTopCoords[1]), int(billboardTopCoords[0] + billboardSize[0]), int(billboardTopCoords[1] + billboardSize[1])], fill = colorAndBorderData['background'], outline = colorAndBorderData['border/legs'], width = colorAndBorderData['lineWidth'])
+    
+    #draw the sign legs
+    draw.line([int(billboardTopCoords[0] + billboardTextLegPositionsX[0]), int(billboardTopCoords[1] + billboardSize[1]), int(billboardTopCoords[0] + billboardTextLegPositionsX[0]), int(billboardTopCoords[1] + billboardSize[1] + billboardLegHeight)], fill = colorAndBorderData['border/legs'], width = colorAndBorderData['lineWidth'])
+    draw.line([int(billboardTopCoords[0] + billboardTextLegPositionsX[1]), int(billboardTopCoords[1] + billboardSize[1]), int(billboardTopCoords[0] + billboardTextLegPositionsX[1]), int(billboardTopCoords[1] + billboardSize[1] + billboardLegHeight)], fill = colorAndBorderData['border/legs'], width = colorAndBorderData['lineWidth'])
+
+    #draw the text
     draw.text([int(billboardTopCoords[0] + billboardTextPadding[0]), int(billboardTopCoords[1] + billboardTextPadding[1])], currentTime, colorAndBorderData['foreground'], font = font)
+
+    #crop the image to be just as big as the sign
+    image = image.crop([int(billboardTopCoords[0] - billboardTextPadding[0]), int(billboardTopCoords[1] - billboardTextPadding[1]), int(billboardTopCoords[0] + billboardTextPadding[0] + billboardSize[0]), int(billboardTopCoords[1] + billboardTextPadding[1] + billboardSize[1] + billboardLegHeight)])
 
     #save the image
     image.save(path)
